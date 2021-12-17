e646d09d6225591c2b802bfebdf41735bb3da5c9
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-12-17 15:43:54 +0900

    test

--- app.js
+++ app.js
@@ -97,7 +97,7 @@ app.get("/pokemonitem", function (req, res) {
   const pokemonmodel = mongoose.model("items", itemSchema);
   const items = new pokemonmodel();
   pokemonmodel.find({_id:"61bc2ce2338139b11055ec6f"},function (error, result) {
-    res.send(result);
+    res.send(result[0]);
   });
 });
 

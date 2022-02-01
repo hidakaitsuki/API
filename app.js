b8f25543fa62ee5fdd89f6195112be348752b16e
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2022-02-02 00:15:13 +0900

    test

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 // 「body-parser」の代わり
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// アクセスを許可するcors
 app.use(cors());
 // mongoDBを操作する「mongoose」をインポート
 const mongoose = require("mongoose");
@@ -43,7 +44,7 @@ const Usermodel = mongoose.model("users", userSchema);
 
 //管理者登録
 app.post("/register", async function (req, res) {
-  // アドレスが既に登録済みかどうか確認
+  // アドレスが既に登録済みかどうか確認（resultにDBからの結果が返ってくる）
   Usermodel.find({ email: req.body.email }, function (err, result) {
     //   既に存在する場合
     if (result.length !== 0) {
@@ -66,13 +67,12 @@ app.post("/register", async function (req, res) {
 });
 // ログイン
 app.post("/login", async function (req, res) {
-  
   //   userコレクションに送信するemailとpassの組み合わせが存在するか確認
   Usermodel.find(
     // AND検索
     { email: req.body.email, pass: req.body.pass },
     function (err, result) {
-      // 存在する場合
+      // 存在する場合ログイン成功
       if (result.length !== 0) {
         res.send({
           data: req.body,
@@ -93,20 +93,20 @@ app.post("/logout", function (req, res) {
   res.send({ status: 200, messsage: "ログアウトに成功しました" });
 });
 
-// スキーマは外に出す！
-const itemSchema =new mongoose.Schema({
+// スキーマは外に出す
+const itemSchema = new mongoose.Schema({
   _id: String,
   item: Array,
   __v: Number,
 });
+// ポケモンの持ち物を取得する
 app.get("/pokemonitem", function (req, res) {
   const pokemonmodel = mongoose.model("items", itemSchema);
   pokemonmodel.find({}, function (error, result) {
     res.send(result[0]);
   });
 });
-
-
+// 持ち物コレクション作成用
 app.post("/pokemonitem", function (req, res) {
   const itemSchema = mongoose.Schema({ item: Array });
   const pokemonmodel = mongoose.model("item", itemSchema);
@@ -116,13 +116,20 @@ app.post("/pokemonitem", function (req, res) {
   items.save();
 });
 
-// mongoose.connect("mongodb://localhost:27017/testdb", { useNewUrlParser: true });
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   age: Number,
+app.post("/line", function (req, res) {
+  res.send(req.body);
+});
+
+require("dotenv").config();
+app.get("/test", function (req, res) {
+  res.send(process.env.NAME);
+});
+// ショッピングカート
+// const cartShema = mongoose.Schema({ address: String }, { items: Array });
+// app.post("/shoppingCart", function (req, wqs) {
+//   const cartmodel = mongoose.model("shoppingcart", cartShema);
+//   const cartitems = new cartmodel();
+
+//   cartitems.address = req.body.address;
+//   cartitems.items = req.body.items;
 // });
-// const Usermodel = mongoose.model("User", userSchema);
-// const user = new Usermodel();
-// user.name = "yamada";
-// user.age = 20;
-// user.save();
